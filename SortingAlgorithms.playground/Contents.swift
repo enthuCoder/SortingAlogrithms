95601d41f437ebef8159ed07e45b8dd1c4b31718
Dilgir Siddiqui <dsiddiqui@apple.com>
2019-10-05 12:53:59 -0700

    Bubble Sort, Insertion Sort, Selection Sort

--- SortingAlgorithms.playground/Contents.swift
+++ SortingAlgorithms.playground/Contents.swift
@@ -1,1 +1,27 @@
 import Cocoa
+
+// -------  -------  -------  -------  -------  //
+// Quick Sort
+// -------  -------  -------  -------  -------  //
+let sorting = Sorting()
+
+var quickSortInput: [Int] = [17, 41, 5, 22, 54, 6, 29, 3, 13]
+sorting.quickSort(input: &quickSortInput)
+print("=-=-=-=-  Quick Sort  -=-=-=-=")
+print("===== SortedList=\(quickSortInput)\n")
+
+// -------  -------  -------  -------  -------  //
+// Insertion Sort
+// -------  -------  -------  -------  -------  //
+let input: [Int] = [5,1,4,2,8]
+let insSortedList = sorting.insertionSort(input: input)
+print("=-=-=-=-  INSERTION Sort  -=-=-=-=")
+print("Input=\(input) ===== SortedList=\(insSortedList)\n")
+
+// -------  -------  -------  -------  -------  //
+// Selection Sort
+// -------  -------  -------  -------  -------  //
+var selectionSortInput: [Int] = [17, 41, 5, 22, 54, 6, 29, 3, 13]
+selectionSortInput = sorting.selectionSort(input: selectionSortInput)
+print("=-=-=-=-  Selection Sort  -=-=-=-=")
+print("===== SortedList=\(selectionSortInput)\n")
--- SortingAlgorithms.playground/Sources/Sorting.swift
+++ SortingAlgorithms.playground/Sources/Sorting.swift
@@ -1,2 +1,103 @@
 import Foundation
 
+// -------  -------  -------  -------  -------  //
+// Common functions needed for Sorting
+// -------  -------  -------  -------  -------  //
+protocol SortUtils {
+    func swap(_ input: inout [Int], swapIndexLeft: Int, swapIndexRight: Int)
+}
+
+extension SortUtils {
+    
+    func swap(_ input: inout [Int], swapIndexLeft: Int, swapIndexRight: Int) {
+        let tempElement = input[swapIndexLeft]
+        input[swapIndexLeft] = input[swapIndexRight]
+        input[swapIndexRight] = tempElement
+    }
+}
+
+public struct Sorting: SortUtils {
+    
+    public init() {}
+    
+    // -------  -------  -------  -------  -------  //
+    // Quick Sort
+    // -------  -------  -------  -------  -------  //
+
+    public func quickSort(input: inout [Int]) {
+        sort(inputArr: &input, lowIndex: 0, highIndex: input.count-1)
+    }
+    
+    private func sort(inputArr: inout [Int], lowIndex: Int, highIndex: Int) {
+        if lowIndex < highIndex {
+            
+            let pi = partition(arr: &inputArr, low: lowIndex, high: highIndex)
+            
+            sort(inputArr: &inputArr, lowIndex: lowIndex, highIndex: pi - 1)
+            sort(inputArr: &inputArr, lowIndex: pi + 1, highIndex: highIndex)
+        }
+    }
+    
+    private func partition(arr: inout [Int], low: Int, high: Int) -> Int {
+        var i = low - 1
+        var j = low
+        
+        while j < high {
+            if arr[j] < arr[high] {
+                i = i + 1
+                swap(&arr, swapIndexLeft: i, swapIndexRight: j)
+            }
+            j = j + 1
+        }
+        arr.insert(arr.remove(at: high), at: i+1)
+        return i+1
+    }
+
+    // -------  -------  -------  -------  -------  //
+    // Insertion Sort
+    // -------  -------  -------  -------  -------  //
+
+    public func insertionSort(input: [Int]) -> [Int] {
+        var result = input
+        
+        for (i, _) in input[1..<input.count].enumerated() {
+            for (j, _) in input[0..<i].enumerated() {
+                print("\n i:\(i) j:\(j)")
+                if result[j] > result[i] {
+                    let removedElement = result.remove(at: i)
+                    result.insert(removedElement, at: j)
+                    print("\(i):: \(result)")
+                }
+            }
+        }
+        
+        return result
+    }
+
+    // -------  -------  -------  -------  -------  //
+    // Selection Sort
+    // -------  -------  -------  -------  -------  //
+    public func selectionSort(input: [Int]) -> [Int] {
+        var result = input
+        var swapIndex = -1
+        var swapNeeded = false
+        
+        for outerIndex in 0..<input.count {
+            swapIndex = outerIndex
+            for innerIndex in outerIndex..<input.count {
+                if result[innerIndex] < result[swapIndex] {
+                    swapIndex = innerIndex
+                    swapNeeded = true
+                }
+            }
+            if swapNeeded == true {
+                swap(&result, swapIndexLeft: outerIndex, swapIndexRight: swapIndex)
+            }
+            swapIndex = -1
+            swapNeeded = false
+        }
+        
+        return result
+    }
+
+}
